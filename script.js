27d1c485af2be734e124f9b37432155edc41448d
DavidCarG <davidzone23@gmail.com>
2022-12-11 11:29:24 -0600

    Create a funciont that plays a single round of Rock,Paper,Scissors

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ function getComputerChoice(){
     return choices[randomNumber(0,2)];
 }
 
+function playGame(userChoice,computerChoice){
+    if(userChoice===computerChoice){
+        return "It's a Draw!";
+    }else if(userChoice==="rock"){
+        if(computerChoice==="paper")
+            return "You lose! Paper covers Rock.";
+        else if(computerChoice==="scissors")
+            return "You win! Rock smashes Scissors.";
+    }else if(userChoice==="paper"){
+        if(computerChoice==="rock")
+            return "You win! Paper covers Rock.";
+        else if(computerChoice==="scissors")
+            return "You lose! Scissors cut Paper.";
+    }else if(userChoice==="scissors"){
+        if(computerChoice==="rock")
+            return "You lose! Rock smashes Scissors.";
+        else if(computerChoice==="paper")
+            return "You win! Scissors cut Paper.";
+    }else{
+        return "Invalid move!";
+    }
+}
+
