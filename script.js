30761c78b91041e9c50682a66d25424546f73016
DavidCarG <davidzone23@gmail.com>
2022-12-11 11:18:28 -0600

    Create a function that returns a random choice between Rock,Paper and Scissors

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const choices = new Array("rock","paper","scissors");
 function randomNumber(min,max){
     return Math.round(Math.random()*(max-min))+min;
 }
+
+function getComputerChoice(){
+    return choices[randomNumber(0,2)];
+}
+
